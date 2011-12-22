844714b2c611992802f431eb960353b8fb692a1a
c9s <cornelius.howl@gmail.com>
2011-12-22 18:10:30 +0800

    Fix checkbox value bug.

--- Phifty/FormWidget/Checkbox.php
+++ Phifty/FormWidget/Checkbox.php
@@ -7,10 +7,21 @@ class Checkbox extends FormWidget
     public function template()
     {
         return <<<CODE
+    <input type="hidden"
+        id="checkbox-{{ column.name }}"
+        name="{{ column.name }}"
+        value="{{ column.value }}"
+    />
+
     <input type="checkbox" 
-            name="{{ column.name }}" 
             {% for attr_name,attr_value in attrs %} {{attr_name}}="{{attr_value}}" {% endfor %}
-            {% if column.value %} checked {% endif %} value="1"/>
+            {% if column.value %} checked {% endif %}
+            value="1" 
+        onclick=" (function(btn) {
+            var c = $(btn).attr('checked');
+            $('#checkbox-{{ column.name }}').val( c ? 1 : 0 );
+        })(this);"
+    />
 CODE;
     }
 
