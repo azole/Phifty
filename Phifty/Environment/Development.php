4a080b02e18ce08662bec2918ed08a200d8febe1
c9s <cornelius.howl@gmail.com>
2011-12-25 19:50:58 +0800

    Separate environment init scripts

--- Phifty/Environment/Development.php
+++ Phifty/Environment/Development.php
@@ -12,7 +12,31 @@ namespace Phifty\Environment;
 
 class Development 
 {
-    
+    static function init($app)
+    {
+        if( ! class_exists( 'ReflectionObject' ) )
+            throw new Exception('ReflectionObject class is not defined. Seems you are running an oooold php.');
+        error_reporting(E_ALL | E_STRICT | E_ERROR | E_NOTICE | E_WARNING | E_PARSE);
+
+        /**
+         * init firephp for development:
+         * http://www.firephp.org/HQ/Use.htm
+         *
+         * if not in CLI mode, include firePHP.
+         **/
+        if( ! $app->isCLI ) {
+            require PH_ROOT . '/dists/firephp/lib/FirePHPCore/fb.php';
 
 
+        /* check configs */
+        /* check php required extensions */
+        $configExt = $app->config('php.extension');
+        if( $configExt ) {
+            foreach( $configExt as $extName ) {
+                if( ! extension_loaded( $extName ) )
+                    throw new \Exception("Extension $extName is not loaded.");
+            }
+        }
+
+    }
 }
---
+++ Phifty/Environment/Production.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This file is part of the Phifty package.
+ *
+ * (c) Yo-An Lin <cornelius.howl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+namespace Phifty\Environment;
+
+class Production
+{
+    static function init($app)
+    {
+        // if we are in command-line mode, 
+        /* for production mode */
+        if( extension_loaded('xdebug') )
+            xdebug_disable();
+        error_reporting(0);
+
+        set_exception_handler( function($e) use ($app) {
+            $subject = 'ERROR: ' . $app->getAppName() . ' - ' . $e->getMessage();
+            // $to = 'cornelius.howl@gmail.com';
+            // $content = '';
+            // $content .= print_r( $e, true ) . "\n";
+            // $content .= print_r( $_SERVER, true ) . "\n";
+            // $content .= print_r( $_REQUEST, true ) . "\n";
+            // $content .= print_r( $_SESSION, true ) . "\n";
+            // xxx: show an error page of this
+            mail( $to , $subject , $content );
+        });
+    }
+}
+
