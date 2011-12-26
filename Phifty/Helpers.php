e3c66a64cfa651fb776548439e84c0daa8fdb45d
c9s <cornelius.howl@gmail.com>
2011-12-26 11:41:31 +0800

    Clean up functions

--- Phifty/Helpers.php
+++ Phifty/Helpers.php
@@ -5,13 +5,3 @@ function webapp()
 {
     return AppKernel::getInstance();
 }
-
-function web_debug( $msg )
-{
-    echo "<div>$msg</div>\n";
-}
-
-
-/* export webapp to global variable */
-global $webapp;
-$webapp = AppKernel::getInstance();
