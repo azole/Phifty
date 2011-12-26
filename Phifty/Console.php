4483f3554debd14c285b384df2397abf22ee7d09
c9s <cornelius.howl@gmail.com>
2011-12-26 13:04:42 +0800

    Add src/Phifty/Console

---
+++ Phifty/Console.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the Phifty package.
+ *
+ * (c) Yo-An Lin <cornelius.howl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Phifty;
+
+class Console extends \CLIFramework\Application
+{
+    static function getInstance()
+    {
+        static $instance;
+        if( $instance )
+            return $instance;
+        return $instance = new static;
+    }
+
+}
+
