78dba75d60ad8aaaffee7506f13d440b2d2ab9fe
c9s <cornelius.howl@gmail.com>
2012-03-02 04:55:16 +0800

    try to fix action column convert.

--- Phifty/Action/ColumnConvert.php
+++ Phifty/Action/ColumnConvert.php
@@ -10,14 +10,18 @@ class ColumnConvert
     static function toParam( $column , $record = null )
     {
 		$name = $column->name;
+
         $param = new \Phifty\Action\Column( $name );
 
         foreach( $column->attributes as $k => $v ) {
-            $param->$attr = $column->$attr;
+            $param->$k = $v;
         }
 
+        $param->name  = $name;
+        $param->label = @$column->label;
+
 		if( $record ) {
-            $param->value( $record->{$name} );
+            $param->value = $record->{$name};
 		}
 
         /* convert column type to param type
