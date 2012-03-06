6f80cdcb1cfa986035c28bdafd585140624d9a77
c9s <cornelius.howl@gmail.com>
2012-03-07 00:56:21 +0800

    Use UniversalRequirementRequirement

--- Phifty/Environment/Development.php
+++ Phifty/Environment/Development.php
@@ -1,14 +1,18 @@
 <?php
-/*
+/**
+ *
  * This file is part of the Phifty package.
  *
  * (c) Yo-An Lin <cornelius.howl@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
  */
 namespace Phifty\Environment;
+use Universal\Requirement\Requirement;
+
+
+
 
 class Development 
 {
@@ -19,7 +23,6 @@ class Development
 
         if( ! class_exists( 'ReflectionObject' ) )
             throw new Exception('ReflectionObject class is not defined. Seems you are running an oooold php.');
-
 
 
         error_reporting(E_ALL | E_STRICT | E_ERROR | E_NOTICE | E_WARNING | E_PARSE);
@@ -34,6 +37,12 @@ class Development
             require_once PH_ROOT . '/vendor/firephp/lib/FirePHPCore/fb.php';
         }
 
+
+        // xxx: can use universal requirement checker 
+        // $req = new Universal\Requirement\Requirement;
+        // $req->extensions( 'apc','mbstring' );
+        // $req->classes( 'ClassName' , 'ClassName2' );
+        // $req->functions( 'func1' , 'func2' , 'function3' )
 
         /* check configs */
         /* check php required extensions */
