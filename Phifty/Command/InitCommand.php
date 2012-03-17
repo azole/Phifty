ad892afc0d7f711d24d9da66c64419900d17b7c4
c9s <cornelius.howl@gmail.com>
2012-03-18 02:32:39 +0800

    Fix init command.

--- Phifty/Command/InitCommand.php
+++ Phifty/Command/InitCommand.php
@@ -1,18 +1,19 @@
 <?php
 namespace Phifty\Command;
-
+use CLIFramework\Command;
 use Phifty\FileUtils;
 use Phifty\CodeTemplate;
 
-class Init extends \Phifty\Command
+class InitCommand extends Command
 {
-    function run()
+
+    function execute()
     {
         $kernel = webapp();
 
-		$this->log( "Initializing phifty dirs..." );
+		$this->logger->info( "Initializing phifty dirs..." );
 
-		$this->log( "Webroot: " . $kernel->getRootDir() );
+		$this->logger->info( "Webroot: " . $kernel->getRootDir() );
 
         $dirs = array();
         $dirs[] = FileUtils::path_join( $kernel->getRootDir() , 'cache' , 'view' );
@@ -43,13 +44,13 @@ class Init extends \Phifty\Command
         $webrootIndex = $kernel->getWebRootDir() . DIRECTORY_SEPARATOR . 'index.php';
         $codegen->renderFile( $webrootIndex , 'webroot_index.php' );
 
-		$this->log( "Changing permissions..." );
+		$this->logger->info( "Changing permissions..." );
         $chmods = array();
         $chmods[] = array( "ga+rw" , "cache" );
         $chmods[] = array( "ga+rw" , $kernel->getCoreWebDir() );
         $chmods[] = array( "ga+rw" , $kernel->getWebRootDir() . DIRECTORY_SEPARATOR . 'static' . DIRECTORY_SEPARATOR . 'upload' );
         foreach( $chmods as $mod ) {
-			$this->log( "\t{$mod[0]} {$mod[1]}" );
+			$this->logger->info( "{$mod[0]} {$mod[1]}", 1 );
             system("chmod -R {$mod[0]} {$mod[1]}");
         }
 
