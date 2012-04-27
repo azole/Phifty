22c21e8039c6487060c182f51c718dff1ba32a61
c9s <cornelius.howl@gmail.com>
2012-04-27 23:20:40 +0800

    register asset as an accessor object.

--- Phifty/Service/AssetService.php
+++ Phifty/Service/AssetService.php
@@ -12,19 +12,26 @@ class AssetService
     }
 
 
+    /**
+     *
+     * $kernel->asset->loader
+     * $kernel->asset->writer
+     */
     function register($kernel, $options = array() ) 
     {
         $config = new AssetKit\Config( PH_APP_ROOT . '/.assetkit' );
-        $kernel->assetLoader = function($kernel) use ($config) {
-            return new AssetKit\AssetLoader( $config );
-        };
-        $kernel->assetWriter = function($kernel) use ($config) {
+        $kernel->asset = function($kernel) use ($config) {
+            $loader = new AssetKit\AssetLoader($config),
             $writer = new AssetKit\AssetWriter($config);
             $writer->env($kernel->environment);
             if( $kernel->namespace ) {
                 $writer->name($kernel->namespace);
             }
-            return $writer;
+
+            return (object) array( 
+                'loader' => $loader,
+                'writer' => $writer,
+            );
         };
     }
 
