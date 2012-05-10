3ec776336c2a83461445207b0cd68148cb1982a3
c9s <cornelius.howl@gmail.com>
2012-05-10 16:12:50 +0800

    build config file (application.yml)

--- Phifty/Command/BuildConfCommand.php
+++ Phifty/Command/BuildConfCommand.php
@@ -24,7 +24,14 @@ class BuildConfCommand extends Command
 
         // should we scan config directories ?
         if( empty($configPath) ) {
-            $configPath = glob('config/*.yml');
+            $configPath = array_filter(
+                array(
+                    'config/application.yml',
+                    'config/framework.yml',
+                    'config/database.yml' 
+                ), function($file) {
+                            return file_exists($file);
+                        });
         }
 
         if( empty($configPath) ) {
