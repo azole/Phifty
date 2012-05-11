da90ee811eec7c651ea96bacb63ebfdb4ee5ad26
c9s <cornelius.howl@gmail.com>
2012-05-11 22:21:12 +0800

    Add ConsoleCommand class

---
+++ Phifty/Command/ConsoleCommand.php
@@ -0,0 +1,17 @@
+<?php
+namespace Phifty\Command;
+use CLIFramework\Command;
+
+class ConsoleCommand extends Command
+{
+
+    function brief() { return 'Simple REPL Console.'; }
+    
+    function execute()
+    {
+
+    }
+}
+
+
+
