3e3278120f604d02981565dd440113fa04b50f44
c9s <cornelius.howl@gmail.com>
2012-05-12 01:44:45 +0800

    refactor asset command.

--- Phifty/Command/AssetCommand.php
+++ Phifty/Command/AssetCommand.php
@@ -4,53 +4,45 @@ use CLIFramework\Command;
 
 class AssetCommand extends Command
 {
+
+    function registerAsset($config,$installer,$dir)
+    {
+        $manifestPath = $dir  . DIRECTORY_SEPARATOR . 'manifest.yml';
+        if( ! file_exists($manifestPath)) 
+            throw new Exception( "$manifestPath does not exist." );
+
+        $asset = new \AssetKit\Asset($manifestPath);
+        $asset->config = $config;
+        $asset->initResource(true); // update it
+
+        $installer->install( $asset );
+
+        $export = $asset->export();
+        $config->addAsset( $asset->name , $export );
+
+        $this->logger->info("Saving config...");
+        $config->save();
+    }
+
+
     function execute() 
     {
         $config = new \AssetKit\Config('.assetkit');
         $kernel = kernel();
 
+        $installer = new \AssetKit\Installer;
 
         $this->logger->info("Finding assets from applications...");
         foreach( $kernel->applications as $application ) {
-            foreach( $plugin->getAssetDirs() as $dir ) {
-                $manifestPath = $dir  . DIRECTORY_SEPARATOR . 'manifest.yml';
-                if( ! file_exists($manifestPath)) 
-                    throw new Exception( "$manifestPath does not exist." );
-
-                $asset = new \AssetKit\Asset($manifestPath);
-                $asset->config = $config;
-                $asset->initResource(true); // update it
-
-                $installer = new \AssetKit\Installer;
-                $installer->install( $asset );
-
-                $export = $asset->export();
-                $config->addAsset( $asset->name , $export );
-
-                $this->logger->info("Saving config...");
-                $config->save();
+            foreach( $application->getAssetDirs() as $dir ) {
+                $this->registerAsset($config,$installer,$dir);
             }
         }
 
         $this->logger->info("Finding assets from plugins...");
         foreach( $kernel->plugins as $plugin ) {
             foreach( $plugin->getAssetDirs() as $dir ) {
-                $manifestPath = $dir  . DIRECTORY_SEPARATOR . 'manifest.yml';
-                if( ! file_exists($manifestPath)) 
-                    throw new Exception( "$manifestPath does not exist." );
-
-                $asset = new \AssetKit\Asset($manifestPath);
-                $asset->config = $config;
-                $asset->initResource(true); // update it
-
-                $installer = new \AssetKit\Installer;
-                $installer->install( $asset );
-
-                $export = $asset->export();
-                $config->addAsset( $asset->name , $export );
-
-                $this->logger->info("Saving config...");
-                $config->save();
+                $this->registerAsset($config,$installer,$dir);
             }
         }
     }
