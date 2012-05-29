81a960c6d66d4fd8c17a138307fcd090c89c88c7
c9s <cornelius.howl@gmail.com>
2012-05-29 23:31:06 +0800

    Basic Action class generator

--- Phifty/Command/Generate/GenerateActionCommand.php
+++ Phifty/Command/Generate/GenerateActionCommand.php
@@ -1,6 +1,7 @@
 <?php
 namespace Phifty\Command\Generate;
 use CLIFramework\Command;
+use ActionKit\ActionGenerator;
 
 class GenerateActionCommand extends Command
 {
@@ -16,9 +17,23 @@ class GenerateActionCommand extends Command
 
         $dir = $app->locate();
         $className = $ns . '\\Action\\' . $actionName;
+        $actionDir = $dir . DIRECTORY_SEPARATOR . 'Action';
+        $classFile = $dir . DIRECTORY_SEPARATOR . 'Action' . DIRECTORY_SEPARATOR . $actionName . '.php';
 
-        kernel()->action->
-        $gen = new ActionGenerator(array( 'cache' => true ));
+        if( ! file_exists($actionDir) ) {
+            mkdir($actionDir, 0755, true);
+        }
+
+        $relfilepath = substr($classFile,strlen(getcwd()) + 1);
+        if( ! file_exists($classFile) ) {
+            $gen = new ActionGenerator(array( 'cache' => true ));
+            $ret = $gen->generateActionClassCode( $ns , $actionName );
+            file_put_contents( $classFile , "<?php\n" . $ret->code . "\n\n?>" );
+            $this->logger->info( $ret->action_class 
+                . ' is generated at ' . $relfilepath );
+        } else {
+            $this->logger->warn( $relfilepath . ' class file exists.' );
+        }
     }
 
 
