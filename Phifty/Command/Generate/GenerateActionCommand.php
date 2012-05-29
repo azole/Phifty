f227906af475171e0899a866a755611628fb6d92
c9s <cornelius.howl@gmail.com>
2012-05-29 23:23:26 +0800

    Test generateActionClassCode

--- Phifty/Command/Generate/GenerateActionCommand.php
+++ Phifty/Command/Generate/GenerateActionCommand.php
@@ -5,5 +5,23 @@ use CLIFramework\Command;
 class GenerateActionCommand extends Command
 {
 
+    function brief() { return 'generate action class'; }
+
+    function usage() { return '[application name|plugin name] [action name]'; }
+
+    function execute($ns,$actionName) 
+    {
+        if( $app = kernel()->app($ns) ) { } 
+        elseif( $app = kernel()->plugin($ns) ) { }
+
+        $dir = $app->locate();
+        $className = $ns . '\\Action\\' . $actionName;
+
+        kernel()->action->
+        $gen = new ActionGenerator(array( 'cache' => true ));
+    }
+
+
+
 }
 
