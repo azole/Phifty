6dc6dd0ee691dc043d45585d19b939f70bef9b7f
c9s <cornelius.howl@gmail.com>
2012-06-27 23:48:35 +0800

    Update changelogs

--- Phifty/Service/TwigService.php
+++ Phifty/Service/TwigService.php
@@ -2,17 +2,17 @@
 namespace Phifty\Service;
 
 class TwigService
-	implements ServiceInterface
+    implements ServiceInterface
 {
     public function getId() { return 'Twig'; }
 
-	public function register($kernel, $options = array() )
-	{
-		$kernel->classloader->addPrefix(array(
-			'Twig_Extensions_'   => $kernel->frameworkDir . '/vendor/twig-extensions/lib',
-			'Twig_'              => $kernel->frameworkDir . '/vendor/pear',
-		));
-	}
+    public function register($kernel, $options = array() )
+    {
+        $kernel->classloader->addPrefix(array(
+            'Twig_Extensions_'   => $kernel->frameworkDir . '/vendor/twig-extensions/lib',
+            'Twig_'              => $kernel->frameworkDir . '/vendor/pear',
+        ));
+    }
 }
 
 
