e325ca8380c60f74b55198fe37aa1c26f701b414
c9s <cornelius.howl@gmail.com>
2012-06-27 21:33:19 +0800

    Migrate CurrentUser config to service config. Issue #112

--- Phifty/Service/CurrentUserService.php
+++ Phifty/Service/CurrentUserService.php
@@ -2,19 +2,25 @@
 namespace Phifty\Service;
 
 class CurrentUserService
-	implements ServiceInterface
+    implements ServiceInterface
 {
 
     public function getId() { return 'current_user'; } 
 
-	public function register($kernel,$options = array() )
-	{
-		// current user builder
-        $kernel->currentUser = function() use ($kernel) {
-            if( $currentUserClass = $kernel->config->get('framework','CurrentUser.Class') )
-                return new $currentUserClass;
+    public function register($kernel,$options = array() )
+    {
+        // current user builder
+        $kernel->currentUser = function() use ($kernel,$options) {
+            // framework.CurrentUser.Class is for backward compatible.
+            $modelClass = isset($options['Model'])
+                ? $options['Model']
+                : $kernel->config->get('framework','CurrentUser.Model');
+            $currentUserClass = isset($options['Class'])
+                ? $options['Class']
+                : $kernel->config->get('framework','CurrentUser.Class') ?: 'Phifty\CurrentUser';
+            return new $currentUserClass;
         };
-	}
+    }
 }
 
 
