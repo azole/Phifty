a96c9599120a7b38fa1a641c08621d60fe94bfd2
c9s <cornelius.howl@gmail.com>
2012-07-16 17:00:55 +0800

    Add SeleniumTestCase

---
+++ Phifty/Testing/SeleniumTestCase.php
@@ -0,0 +1,19 @@
+<?php
+namespace Phifty\Testing;
+use PHPUnit_Extensions_Selenium2TestCase;
+
+class SeleniumTestCase extends PHPUnit_Extensions_Selenium2TestCase
+{
+    public $kernel;
+
+    function setUp() {
+        $this->kernel = kernel();
+
+        $config = $this->kernel->config('framework');
+
+        $this->setBrowserUrl('http://www.example.com/');
+    }
+
+}
+
+
