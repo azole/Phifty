8fe46f49a35e04ae603eee6077813cd4b2cf82aa
c9s <cornelius.howl@gmail.com>
2012-07-20 21:52:00 +0800

    Add Phifty\Notification

---
+++ Phifty/Notification/NotificationQueue.php
@@ -0,0 +1,12 @@
+<?php
+namespace Phifty\Notification;
+use ZMQ;
+
+class NotificationQueue
+{
+    public function __construct($bind) {
+
+    }
+}
+
+// new ZMQ(new ZMQContext(), ZMQ::SOCKET_REQ, "MySock1");
---
+++ Phifty/Notification/NotificationServer.php
@@ -0,0 +1,48 @@
+<?php
+namespace Phifty\Notification;
+use ZMQ;
+use ZMQSocket;
+use ZMQContext;
+use ZMQSocketException;
+
+class NotificationServer
+{
+    public $context;
+
+    public $responder;
+
+    public $publiser;
+
+    function __construct()
+    {
+    }
+
+    function connect($bind,$publishPoint) {
+        $this->context = new ZMQContext(1);
+
+        //  Socket to talk to clients
+        $this->responder = new ZMQSocket($this->context, ZMQ::SOCKET_REP);
+        $this->responder->bind($bind);
+
+        $this->publisher = new ZMQSocket($this->context, ZMQ::SOCKET_PUB);
+        $this->publisher->bind($publishPoint);
+    }
+
+    function start() {
+        while(true) {
+            //  Wait for next request from client
+            $msg = $this->responder->recv();
+            printf("Received request: [%s]%s", $msg, PHP_EOL);
+
+            $this->publisher->send($msg);
+
+            //  Send reply back to client
+            $this->responder->send('{"success":1}');
+        }
+    }
+}
+
+
+
+
+
