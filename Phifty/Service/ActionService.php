10ec8ed41312ce0ce95d709f4464f35650419fed
c9s <cornelius.howl@gmail.com>
2012-07-27 11:35:26 +0800

    Mount ActionRunner in before_path_dispatch

--- Phifty/Service/ActionService.php
+++ Phifty/Service/ActionService.php
@@ -23,7 +23,7 @@ class ActionService
 
         $kernel->classloader->addNamespace(array( 'ActionKit' => array( $kernel->frameworkDir . DIRECTORY_SEPARATOR . 'src' ) ));
 
-        $kernel->event->register('phifty.run',function() use ($kernel) {
+        $kernel->event->register('phifty.before_path_dispatch',function() use ($kernel) {
 
             // check if there is $_POST['action'] or $_GET['action']
             if( isset($_REQUEST['action']) ) {
