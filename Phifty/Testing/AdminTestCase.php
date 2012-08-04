8dd0e2f20864ba15bec2f43c68d827541a102fb0
EragonJ <eragonj@eragonj.me>
2012-08-04 22:47:43 +0800

    Added: AdminTestCase that extends S2TestCase to make test method clear

---
+++ Phifty/Testing/AdminTestCase.php
@@ -0,0 +1,59 @@
+<?php
+namespace Phifty\Testing;
+
+class AdminTestCase extends Selenium2TestCase 
+{
+    protected $urlOf = [
+        'login' => '',
+        'news' => 'http://phifty.dev/bs/news',
+        'contacts' => 'http://phifty.dev/bs/contacts',
+        'contactGroups' => 'http://phifty.dev/bs/contact_groups'
+    ];
+
+    protected function login( $transferTo='' ) 
+    {
+        // hard code here
+        $this->url('http://phifty.dev/bs/login');
+
+        $accountInput = get('input[name=account]');
+        $accountInput->value('admin');
+
+        $passwordInput = get('input[name=password]');
+        $passwordInput->value('admin');
+
+        get('.submit')->click();
+
+        if ( '' !== $transferTo  ) {
+            $this->url( $this->urlOf[ $transferTo ] );
+        }
+
+        wait();
+    }
+
+    protected function logout()
+    {
+        get('#operation .buttons a[href]')->click();
+        wait();
+    }
+
+    protected function isCreated() {
+
+        $msg = get('.message.success', 5)->text();
+        $o = get_obj(4); 
+        $o->assertContains('created', $msg );
+    }
+
+    protected function isUpdated() {
+
+        $msg = get('.message.success', 5)->text();
+        $o = get_obj(4); 
+        $o->assertContains('updated', $msg );
+    }
+
+    protected function isDeleted() {
+
+        $msg = get('.jGrowl-message', 5)->text();
+        $o = get_obj(4);
+        $o->assertRegExp('/(deleted|刪除成功)/', $msg );
+    }
+}
--- Phifty/Testing/Selenium2TestCase.php
+++ Phifty/Testing/Selenium2TestCase.php
@@ -6,19 +6,13 @@ use PHPUnit_Extensions_Selenium2TestCase;
 abstract class Selenium2TestCase extends PHPUnit_Extensions_Selenium2TestCase 
 {
 
-    protected $urlOf = [
-        'login' => 'http://phifty.dev/bs/login',
-        'news' => 'http://phifty.dev/bs/news',
-        'contacts' => 'http://phifty.dev/bs/contacts',
-        'contactGroups' => 'http://phifty.dev/bs/contact_groups'
-    ];
-
     protected function setUp()
     {
         $kernel = kernel();
         $kernel->config->load('testing','config/testing.yml');
         $config = $kernel->config->get('testing');
         if($config && $config->Selenium) {
+
             if($config->Selenium->Host)
                 $this->setHost($config->Selenium->Host);
 
@@ -33,22 +27,5 @@ abstract class Selenium2TestCase extends PHPUnit_Extensions_Selenium2TestCase
         }
     }
 
-    protected function login( $transferTo='' ) 
-    {
-        $this->url( $this->urlOf['login'] );
-
-        $accountInput = get('input[name=account]');
-        $accountInput->value('admin');
-
-        $passwordInput = get('input[name=password]');
-        $passwordInput->value('admin');
-
-        get('.submit')->click();
-
-        if ( '' !== $transferTo  )
-            $this->url( $this->urlOf[ $transferTo ] );
-
-        wait();
-    }
 }
 
