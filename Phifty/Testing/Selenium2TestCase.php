ac6b2d4c5eb56ea7e9d9a8df1c8670bfa1e9e944
c9s <cornelius.howl@gmail.com>
2012-08-11 23:30:10 +0800

    Add file name parameter to takescreenshot method.

--- Phifty/Testing/Selenium2TestCase.php
+++ Phifty/Testing/Selenium2TestCase.php
@@ -36,13 +36,16 @@ abstract class Selenium2TestCase extends PHPUnit_Extensions_Selenium2TestCase
         }
 
         // XXX: SeleniumTestCase (1.0) seems don't support screenshotPath ?
-        $this->screenshotPath = $this->getScreenshotDir();
+        // $this->screenshotPath = $this->getScreenshotDir();
     }
 
     // Override the original method and tell Selenium to take screen shot when test fails
     public function onNotSuccessfulTest(\Exception $e) 
     {
-        $this->takeScreenShot();
+        $this->takeScreenshot('now.png');
+
+        // use unix-timestamp so that we can sort file by name
+        $this->takeScreenshot( str_replace('.','_',microtime(true)) . '.png' );
         parent::onNotSuccessfulTest($e);
     }
 
@@ -50,21 +53,16 @@ abstract class Selenium2TestCase extends PHPUnit_Extensions_Selenium2TestCase
         return PH_ROOT . '/tests/screenshots'; 
     }
 
-    public function takeScreenShot() 
+    public function takeScreenshot($filename = null)
     {
-        $screenshotPath = $this->getScreenshotDir();
-
-        $screenShot = $this->currentScreenshot();
-
-        if ( !is_string( $screenShot ) || ! $screenShot ) {
-            throw new Exception('Take ScreenShot failed');
+        $image = $this->currentScreenshot();
+        if ( !is_string( $image ) || ! $image ) {
+            throw new Exception('screenshot failed, empty result.');
         }
-        
-        $filePath1 = $screenshotPath . DIRECTORY_SEPARATOR . md5(rand()) . '.png'; // Create an unique file name
-        $filePath2 = $screenshotPath . DIRECTORY_SEPARATOR . 'now.png'; // Create the easily recognized file name
-
-        file_put_contents( $filePath1, $screenShot );
-        file_put_contents( $filePath2, $screenShot );
+        $path = $this->getScreenshotDir() . DIRECTORY_SEPARATOR . $filename;
+        if( file_put_contents( $path , $image ) === false ) {
+            throw new Exception("can not write screenshot image file $path");
+        }
     }
 }
 
