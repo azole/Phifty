74569642c02a3d885adab9e12b77bacc8df80962
c9s <cornelius.howl@gmail.com>
2012-08-13 02:34:39 +0800

    Phifty: Rename MetaSchema to MetadataSchema

--- Phifty/Model/Mixin/MetadataSchema.php
+++ Phifty/Model/Mixin/MetadataSchema.php
@@ -2,7 +2,7 @@
 namespace Phifty\Model\Mixin;
 use LazyRecord\Schema\MixinSchemaDeclare;
 
-class MetaSchema extends MixinSchemaDeclare
+class MetadataSchema extends MixinSchemaDeclare
 {
     function schema()
     {
@@ -10,27 +10,38 @@ class MetaSchema extends MixinSchemaDeclare
             ->timestamp()
             ->renderAs('DateTimeInput')
             ->label( _('Created on') )
+            ->default(function() {
+                return date('c');
+            })
             ;
 
         $this->column( 'updated_on' )
             ->timestamp()
             ->renderAs('DateTimeInput')
+            ->default(function() {
+                return date('c');
+            })
             ->label( _('Updated on') )
             ;
 
         $this->column( 'created_by' )
             ->integer()
+            ->refer( kernel()->currentUser->userModelClass )
             ->default(function() { 
                 return kernel()->currentUser->id; 
             })
+            ->renderAs('SelectInput')
             ->label('建立者')
             ;
 
+        // XXX: inject value to beforeUpdate
         $this->column( 'updated_by' )
             ->integer()
+            ->refer( kernel()->currentUser->userModelClass )
             ->default(function() { 
                 return kernel()->currentUser->id; 
             })
+            ->renderAs('SelectInput')
             ->label('更新者')
             ;
     }
