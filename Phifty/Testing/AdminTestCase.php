5dcfb77f6dac795b7bbc59287d9b478d4d9e574b
EragonJ <eragonj@eragonj.me>
2012-08-14 14:24:58 +0800

    Updated: make function more clear

--- Phifty/Testing/AdminTestCase.php
+++ Phifty/Testing/AdminTestCase.php
@@ -62,13 +62,13 @@ class AdminTestCase extends Selenium2TestCase
 
     protected function isDeleted() 
     {
-        $msg = waitFor('.jGrowl-message')->text();
+        $msg = wait_for('.jGrowl-message')->text();
         $this->assertRegExp('/(deleted|刪除成功)/', $msg );
     }
 
     protected function isUploaded() 
     {
-        $msg = waitFor('.jGrowl-message')->text();
+        $msg = wait_for('.jGrowl-message')->text();
         $this->assertContains('created', $msg );
     }
 
