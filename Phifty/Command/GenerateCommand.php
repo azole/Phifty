7e809a7b29ea7f6535ff8a9efa17e8f7ca9842c7
c9s <cornelius.howl@gmail.com>
2012-08-19 15:25:50 +0800

    Improve GenerateCommand, use GenPHP/Generator

--- Phifty/Command/GenerateCommand.php
+++ Phifty/Command/GenerateCommand.php
@@ -8,12 +8,28 @@ class GenerateCommand extends Command
 {
     function brief() { return 'template generator command'; }
 
-    function init()
-    {
-        $this->registerCommand('action','Phifty\Command\Generate\GenerateActionCommand');
-        $this->registerCommand('model','Phifty\Command\Generate\GenerateModelCommand');
-        $this->registerCommand('controller','Phifty\Command\Generate\GenerateControllerCommand');
-        $this->registerCommand('test','Phifty\Command\Generate\GenerateTestCommand');
+#      function init()
+#      {
+#  #          $this->registerCommand('action','Phifty\Command\Generate\GenerateActionCommand');
+#  #          $this->registerCommand('model','Phifty\Command\Generate\GenerateModelCommand');
+#  #          $this->registerCommand('controller','Phifty\Command\Generate\GenerateControllerCommand');
+#  #          $this->registerCommand('test','Phifty\Command\Generate\GenerateTestCommand');
+#      }
+
+    function execute($flavor) {
+        $args = func_get_args();
+        array_shift($args);
+
+        $loader = new \GenPHP\Flavor\FlavorLoader(array( 
+            PH_ROOT . '/src/Phifty/Flavors'
+        ));
+        if( $flavor = $loader->load($flavor) ) {
+            $runner = new \GenPHP\GeneratorRunner;
+            $runner->run($generator,$args);
+        } else {
+            throw new Exception("Flavor $flavor not found.");
+        }
+        $this->info('Done');
     }
 }
 
