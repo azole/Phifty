33a807169ef496fa374611734dd09f048ce665b5
c9s <cornelius.howl@gmail.com>
2012-08-19 16:30:36 +0800

    Add shema generator

--- Phifty/Flavors/schema/Generator.php
+++ Phifty/Flavors/schema/Generator.php
@@ -4,5 +4,48 @@ use GenPHP\Flavor\BaseGenerator;
 
 class Generator extends BaseGenerator
 {
+    function brief() { return 'generate schema class'; }
+
+    function generate($ns,$schemaName)
+    {
+        $app = kernel()->app($ns) ?: kernel()->plugin($ns);
+        if( ! $app ) {
+            throw new Exception("$ns application or plugin not found.");
+        }
+
+        $this->logger->info("Found $ns");
+
+
+        if( strrpos($schemaName,'Schema') === false ) {
+            $schemaName .= 'Schema';
+        }
+
+        $args = func_get_args();
+        $args = array_splice($args,2);
+        $schemaActions = array('indexAction');
+        foreach( $args as $arg ) {
+            $schemaActions[] = $arg . 'Action';
+        }
+
+        $dir = $app->locate();
+        $className = $ns . '\\Schema\\' . $schemaName;
+        $classDir = $dir . DIRECTORY_SEPARATOR . 'Schema';
+        $classFile = $classDir . DIRECTORY_SEPARATOR . $schemaName . '.php';
+
+        if( ! file_exists($classDir) ) {
+            mkdir($classDir, 0755, true);
+        }
+
+        if( file_exists($classFile) ) {
+            $this->logger->info("Found existing $classFile, skip");
+            return;
+        }
+
+        $this->render('Schema.php.twig',$classFile,array( 
+            'namespace' => $ns,
+            'schemaName' => $schemaName,
+            // 'schemaColumns' => $schemaActions,
+        ));
+    }
 
 }
