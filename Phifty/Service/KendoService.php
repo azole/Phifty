a76c9921025082d5ed8cde2152a044a587027666
c9s <cornelius.howl@gmail.com>
2012-08-24 02:51:24 +0800

    use instanceof insteadof is_a

--- Phifty/Service/KendoService.php
+++ Phifty/Service/KendoService.php
@@ -1,16 +1,31 @@
 <?php
 namespace Phifty\Service;
 use Exception;
+use Kendo\Acl\RuleLoader;
+use Kendo\Acl\Acl;
 
 class KendoService
     implements ServiceInterface
 {
+
+    /**
+     * The RuleLoader
+     */
+    public $loader;
+
     public function getId() { return 'KendoAccessControl'; }
 
     public function register($kernel,$options = array())
     {
         $self = $this;
-        $kernel->acl = function() use($self,$kernel) { };
+        $kernel->acl = function() use($self,$kernel,$options) {
+            $loader = new RuleLoader;
+            foreach( $options->Rules as $ruleClass ) {
+                $loader->load($ruleClass);
+            }
+            $acl = new Acl($loader);
+            return $acl;
+        };
     }
 }
 
