cb5a7215e46810d0e8860e7198b3fb96df977859
c9s <cornelius.howl@gmail.com>
2012-09-07 15:18:02 +0800

    mkdir for plugin

--- Phifty/Flavors/plugin/Generator.php
+++ Phifty/Flavors/plugin/Generator.php
@@ -8,7 +8,12 @@ class Generator extends BaseGenerator
 
     function generate($pluginName) 
     {
-
+        $pluginDir = PH_APP_ROOT . DIRECTORY_SEPARATOR . 'plugins' . DIRECTORY_SEPARATOR . $pluginName;
+        $this->mkdir( $pluginDir );
+        $this->mkdir( $pluginDir . DIRECTORY_SEPARATOR . 'Model' );
+        $this->mkdir( $pluginDir . DIRECTORY_SEPARATOR . 'Controller' );
+        $this->mkdir( $pluginDir . DIRECTORY_SEPARATOR . 'Action' );
+        $this->mkdir( $pluginDir . DIRECTORY_SEPARATOR . 'template' );
+        $this->mkdir( $pluginDir . DIRECTORY_SEPARATOR . 'assets' );
     }
-
 }
