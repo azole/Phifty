bb8ed396d2ca4cb3d8e12077d64c54f75b9073e9
c9s <cornelius.howl@gmail.com>
2012-09-07 15:16:40 +0800

    Add plugin structure generator

---
+++ Phifty/Flavors/plugin/Generator.php
@@ -0,0 +1,14 @@
+<?php
+namespace plugin;
+use GenPHP\Flavor\BaseGenerator;
+
+class Generator extends BaseGenerator
+{
+    function brief() { return 'generate plugin structure'; }
+
+    function generate($pluginName) 
+    {
+
+    }
+
+}
