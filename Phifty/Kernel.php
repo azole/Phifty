45dda79c5b30bcec87bacf037c735251c89adf18
c9s <cornelius.howl@gmail.com>
2012-09-19 01:13:54 +0800

    Release 2.1.15
    Version 2.1.15 - Wed Sep 19 01:13:18 2012
    - Add remote logging controller for Tracker

--- Phifty/Kernel.php
+++ Phifty/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ObjectContainer
 {
     /* framework version */
     const FRAMEWORK_ID = 'phifty';
-    const VERSION = '2.1.14';
+    const VERSION = '2.1.15';
 
     public $frameworkDir;
     public $frameworkAppDir;
