7c8f0da7728f20649557b105c7e3236cc836515f
c9s <cornelius.howl@gmail.com>
2012-09-19 16:31:13 +0800

    Release 2.1.17
    Changes
    
    Version 2.1.17 - Wed Sep 19 16:30:37 2012
    
    - IE fix
    - StatsQuery fix

--- Phifty/Kernel.php
+++ Phifty/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ObjectContainer
 {
     /* framework version */
     const FRAMEWORK_ID = 'phifty';
-    const VERSION = '2.1.16';
+    const VERSION = '2.1.17';
 
     public $frameworkDir;
     public $frameworkAppDir;
