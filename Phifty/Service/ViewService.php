30663547a4bdb677a475428f93184165e6e56d96
c9s <cornelius.howl@gmail.com>
2012-09-25 21:06:50 +0800

    Add ViewService

---
+++ Phifty/Service/ViewService.php
@@ -0,0 +1,13 @@
+<?php
+namespace Phifty\Service;
+
+class ViewService
+    implements ServiceInterface
+{
+    public function getId() { return 'View'; }
+    public function register($kernel, $options = array() ) 
+    {
+
+    }
+}
+
