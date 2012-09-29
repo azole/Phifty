cd21eca6a0f6ff5b13327a7bed1e4e737ea920c5
c9s <cornelius.howl@gmail.com>
2012-09-29 16:11:21 +0800

    bump version to 2.1.22

--- Phifty/Kernel.php
+++ Phifty/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ObjectContainer
 {
     /* framework version */
     const FRAMEWORK_ID = 'phifty';
-    const VERSION = '2.1.21';
+    const VERSION = '2.1.22';
 
     public $frameworkDir;
     public $frameworkAppDir;
