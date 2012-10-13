18a5cb1283b7f8ffd14fce3254f54508c0fead05
c9s <cornelius.howl@gmail.com>
2012-10-14 02:35:57 +0800

    Add hintFromSizeInfo and pretty_size helper function

--- Phifty/FileUtils.php
+++ Phifty/FileUtils.php
@@ -5,7 +5,20 @@ use RecursiveDirectoryIterator;
 
 class FileUtils {
 
-    static function path_join($list)
+    public static function pretty_size($bytes)
+    {
+        if( $bytes < 1024 )
+            return $bytes . 'B';
+        if( $bytes < 1024 * 1024 )
+            return ((int) $bytes / 1024) . 'KB';
+        if( $bytes < 1024 * 1024 * 1024 )
+            return ((int) $bytes / 1024 / 1024) . 'MB';
+        if( $bytes < 1024 * 1024 * 1024 * 1024 )
+            return ((int) $bytes / 1024 / 1024 / 1024) . 'GB';
+        return ((int) $bytes / 1024 / 1024) . 'MB';
+    }
+
+    public static function path_join($list)
     {
         $args = null;
         if( is_array($list) ) {
