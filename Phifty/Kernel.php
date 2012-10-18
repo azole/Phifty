6bc9f6e863b6bb12d4895e09b190373821299a51
c9s <cornelius.howl@gmail.com>
2012-10-18 13:37:49 +0800

    bump version for bug fix

--- Phifty/Kernel.php
+++ Phifty/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ObjectContainer
 {
     /* framework version */
     const FRAMEWORK_ID = 'phifty';
-    const VERSION = '2.1.25';
+    const VERSION = '2.1.26';
 
     public $frameworkDir;
     public $frameworkAppDir;
