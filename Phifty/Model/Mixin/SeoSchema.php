54707f03ecf9b4fea077a001c3df607e5a9635c8
c9s <cornelius.howl@gmail.com>
2012-10-18 16:15:32 +0800

    Add SEO Schema Mixin model

---
+++ Phifty/Model/Mixin/SeoSchema.php
@@ -0,0 +1,27 @@
+<?php
+namespace Phifty\Model\Mixin;
+use LazyRecord\Schema\MixinSchemaDeclare;
+
+class SeoSchema extends MixinSchemaDeclare
+{
+    public function schema()
+    {
+
+        $this->column( 'seo_keywords' )
+            ->text()
+            ->label('SEO 關鍵字');
+            ;
+
+        $this->column( 'seo_description' )
+            ->text()
+            ->label('SEO 敘述')
+            ;
+
+        $this->column( 'seo_title' )
+            ->varchar(256)
+            ->label('SEO 標題');
+    }
+}
+
+
+
