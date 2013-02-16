e57e9e4cf245939d856a6ae9069dfd658ee75ace
c9s <cornelius.howl@gmail.com>
2013-02-17 02:06:45 +0800

    Add abstract BaseService class

---
+++ Phifty/Service/BaseService.php
@@ -0,0 +1,20 @@
+<?php
+namespace Phifty\Service;
+
+abstract class BaseService 
+{
+    public $options;
+
+    abstract public function getId();
+
+    /**
+     * register service
+     *
+     * XXX: we should set options in constructor
+     */
+    abstract public function register($kernel, $options = array());
+}
+
+
+
+
