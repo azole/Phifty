f4807458e53e2262a84a177717d712e0cf7c1251
c9s <cornelius.howl@gmail.com>
2013-03-09 21:14:16 +0800

    Improve requirement checker

--- Phifty/Command/RequirementCommand.php
+++ Phifty/Command/RequirementCommand.php
@@ -4,11 +4,20 @@ use CLIFramework\Command;
 
 class RequirementCommand extends Command
 {
-    function printResult($msg, $ok) {
-        printf("% -30s %s\n",$msg . ':', $ok ? 'ok' : 'not ok' );
+
+    public function printResult($msg, $ok) 
+    {
+        $f = $this->formatter;
+        echo sprintf("% -30s ",$msg);
+        if( $ok ) {
+            echo $f->format( "[  OK  ]" , 'success' );
+        } else {
+            echo $f->format( "[ FAIL ]" , 'fail' );
+        }
+        echo PHP_EOL;
     }
 
-    function execute() 
+    public function execute() 
     {
         // xxx: Can use universal requirement checker.
         //
@@ -16,10 +25,22 @@ class RequirementCommand extends Command
         // $req->extensions( 'apc','mbstring' );
         // $req->classes( 'ClassName' , 'ClassName2' );
         // $req->functions( 'func1' , 'func2' , 'function3' )
+        //
+        $exts = array(
+            'apc',
+            'pdo',
+            'mysqli',
+        );
+        foreach( $exts as $ext ) {
+            $this->printResult($ext, extension_loaded($ext) );
+        }
+
         $this->printResult('reflection', class_exists('ReflectionObject') );
         $this->printResult('lazyrecord', class_exists('LazyRecord\BaseModel',true));
-        $this->printResult('assetkit',   class_exists('AssetKit\AssetLoader',true));
+        $this->printResult('assetkit',   class_exists('AssetToolkit\AssetLoader',true));
         $this->printResult('roller',     class_exists('Roller\Router',true));
+
+
         $this->printResult('roller extension', extension_loaded('roller') );
 
         $kernel = kernel();
