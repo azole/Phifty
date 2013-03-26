186b1df36a0870c03dafaae9f102ec03483f015d
c9s <cornelius.howl@gmail.com>
2013-03-27 01:41:51 +0800

    remove unused methods from Plugin object, so taht we can simply use Bundle class.

--- Phifty/Plugin/Plugin.php
+++ Phifty/Plugin/Plugin.php
@@ -11,40 +11,4 @@ class Plugin extends Bundle
     {
         return $this->getNamespace();
     }
-
-    /* static method */
-    public static function locatePlugin( $name )
-    {
-        /* possible plugin paths */
-        $paths = array();
-        $paths[] = FileUtils::path_join( PH_APP_ROOT , 'plugins' , $name );
-        $paths[] = FileUtils::path_join( PH_ROOT , 'plugins' , $name );
-        foreach( $paths as $path ) {
-            if ( file_exists( $path ) ) {
-                return $path;
-            }
-        }
-    }
-
-    /*
-    Use case:
-        kernel()->plugin('SB')->render( 'product/view' , array( .... args .... ) );
-
-    */
-    public function render( $vpath )
-    {
-    }
-
-    /* methods for definitions */
-
-    public function init()
-    {
-        // $this->route( );
-    }
-
-    public function beforePage()
-    {
-
-    }
-
 }
