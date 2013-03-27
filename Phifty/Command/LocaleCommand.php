53a5350cf080d0ee1d6b79c73e9e206fb85e6e3d
c9s <cornelius.howl@gmail.com>
2013-03-27 12:30:24 +0800

    Fix LocaleCommand (to list po files)

--- Phifty/Command/LocaleCommand.php
+++ Phifty/Command/LocaleCommand.php
@@ -1,6 +1,7 @@
 <?php
 namespace Phifty\Command;
 use CLIFramework\Command;
+use Symfony\Component\Finder\Finder;
 
 /**
  *
@@ -12,17 +13,14 @@ use CLIFramework\Command;
  */
 class LocaleCommand extends Command
 {
-
     public function execute()
     {
         $kernel = kernel();
-
-#          $finder = Finder::create()->files()->name('*.po')->in(
-#              PH_ROOT . DIRECTORY_SEPARATOR . $kernel->config->get('framework','Locale.localedir')
-#          );
-#          foreach ( $finder->getIterator() as $file ) {
-#              echo $file, "\n";
-#          }
+        $localeDir = PH_APP_ROOT . DIRECTORY_SEPARATOR . $kernel->config->get('framework','Services.LocaleService.LocaleDir');
+        $finder = Finder::create()->files()->name('*.po')->in( $localeDir );
+        foreach ( $finder->getIterator() as $file ) {
+            echo $file, "\n";
+        }
     }
 
 }
