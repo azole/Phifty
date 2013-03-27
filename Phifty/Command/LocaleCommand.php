32735c6b7538808a36055255b8845ed8adaf74da
c9s <cornelius.howl@gmail.com>
2013-03-27 12:47:03 +0800

    create po files

--- Phifty/Command/LocaleCommand.php
+++ Phifty/Command/LocaleCommand.php
@@ -2,6 +2,7 @@
 namespace Phifty\Command;
 use CLIFramework\Command;
 use Symfony\Component\Finder\Finder;
+use Phifty\Kernel;
 
 /**
  *
@@ -13,14 +14,52 @@ use Symfony\Component\Finder\Finder;
  */
 class LocaleCommand extends Command
 {
+
+    public function options($opts)
+    {
+        $opts->add('f|force','force');
+    }
+
+
     public function execute()
     {
         $kernel = kernel();
-        $localeDir = PH_APP_ROOT . DIRECTORY_SEPARATOR . $kernel->config->get('framework','Services.LocaleService.LocaleDir');
+        $localeDir = $kernel->config->get('framework','Services.LocaleService.Directory') ?: 'locale';
+        $frameworkLocaleDir = PH_ROOT . DIRECTORY_SEPARATOR . 'locale';
+        $langs     = $kernel->config->get('framework','Services.LocaleService.Langs')->config;
+
+        $cwd = getcwd();
         $finder = Finder::create()->files()->name('*.po')->in( $localeDir );
-        foreach ( $finder->getIterator() as $file ) {
-            echo $file, "\n";
+        $appPoFiles = array();
+        $frameworkId = Kernel::FRAMEWORK_ID;
+        $appId       = $kernel->config->framework->ApplicationID;
+
+        $frameworkPoFilename = $frameworkId . '.po';
+        $appPoFilename       = $appId . '.po';
+
+        foreach( $langs as $langId ) {
+            $poDir        = $localeDir . DIRECTORY_SEPARATOR . $langId . DIRECTORY_SEPARATOR . 'LC_MESSAGES';
+            $sourcePoPath = $frameworkLocaleDir . DIRECTORY_SEPARATOR . $langId . DIRECTORY_SEPARATOR . 'LC_MESSAGES' . DIRECTORY_SEPARATOR . $frameworkId . '.po';
+            $targetPoPath = $localeDir . DIRECTORY_SEPARATOR . $langId . DIRECTORY_SEPARATOR . 'LC_MESSAGES' . DIRECTORY_SEPARATOR . $appId . '.po';
+
+            if ( ! file_exists($poDir) ) {
+                mkdir($poDir, 0755, true);
+            }
+
+            if ( $this->options->force || file_exists( $sourcePoPath ) && ! file_exists( $targetPoPath ) ) {
+                $this->logger->info("Creating $targetPoPath");
+                copy($sourcePoPath, $targetPoPath);
+            }
         }
+
+        /*
+        foreach ( $finder->getIterator() as $file ) {
+            $shortPath     = substr( $file->getPath() , strlen($cwd) + 1 );
+            $shortPathname = substr( $file->getPathname() , strlen($cwd) + 1 );
+            $targetPoPathname = $shortPath . DIRECTORY_SEPARATOR . $appPoFilename;
+            $this->logger->info("Found $shortPathname",1);
+        }
+         */
     }
 
 }
