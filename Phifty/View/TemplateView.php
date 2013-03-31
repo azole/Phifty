3d8f1a2705acc22894334bc1fa9acd9abfa2d0cd
c9s <cornelius.howl@gmail.com>
2013-03-31 23:52:50 +0800

    Add Phifty\View\TemplateView

---
+++ Phifty/View/TemplateView.php
@@ -0,0 +1,60 @@
+<?php
+namespace Phifty\View;
+use ReflectionObject;
+use Exception;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
+
+abstract class TemplateView
+{
+    private $_classDir;
+
+    public function getClassDir()
+    {
+        if ( $this->_classDir ) {
+            return $this->_classDir;
+        }
+        $ref = new ReflectionObject($this);
+        return $this->_classDir = dirname($ref->getFilename());
+    }
+
+    public function getTemplateDir()
+    {
+        return $this->getClassDir() . DIRECTORY_SEPARATOR . 'Templates';
+    }
+
+    public function createTwigFileSystemLoader()
+    {
+        $dir = $this->getTemplateDir();
+        if ( ! file_exists($dir) ) {
+            throw RuntimeException("Directory $dir for TemplateView does not exist.");
+        }
+        return new Twig_Loader_Filesystem($dir);
+    }
+
+    public function createTwigEnvironment()
+    {
+        $loader = $this->createTwigFileSystemLoader();
+        return new Twig_Environment($loader, $this->getTwigConfig());
+    }
+
+    public function getTwigConfig()
+    {
+        return array();
+    }
+
+    public function getTemplate($templateFile)
+    {
+        $twig = $this->createTwigEnvironment();
+        return $twig->loadTemplate($templateFile);
+    }
+
+    /* $twig->render('index.html', array('the' => 'variables', 'go' => 'here')); */
+    public function renderTemplateFile($templateFile,$arguments = array())
+    {
+        $template = $this->getTemplate($templateFile);
+        $arguments = array_merge( array('View' => $this ) , $arguments );
+        return $template->render($arguments);
+    }
+}
+
