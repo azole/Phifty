2eef7d6afb9e7e81cdf5f37cd04a6989ee6b2c20
c9s <cornelius.howl@gmail.com>
2013-08-08 13:22:58 +0800

    Add Product RESTful handler

--- Phifty/Service/RouterService.php
+++ Phifty/Service/RouterService.php
@@ -9,17 +9,26 @@ class RouterService
 
     public function register($kernel, $options = array() )
     {
+        $kernel->restful = function() use($kernel) {
+            $restful = new \Roller\Plugin\RESTful(array( 
+                'prefix' => '/restful'
+            ));
+            return $restful;
+        };
         $kernel->router = function() use ($kernel) {
+            $router = null;
             if ('production' === $kernel->environment) {
-                return new Router(null, array(
-                    'route_class' => 'Phifty\Routing\Route',
+                $router = new Router(null, array(
+                    'route_class' => 'Phifty\\Routing\\Route',
                     'cache_id' => PH_APP_ROOT ? PH_APP_ROOT : $kernel->config->get('framework','uuid'),
                 ));
+            } else {
+                $router = new Router(null, array(
+                    'route_class' => 'Phifty\\Routing\\Route',
+                ));
             }
-
-            return new Router(null, array(
-                'route_class' => 'Phifty\Routing\Route',
-            ));
+            $router->addPlugin($kernel->restful);
+            return $router;
         };
     }
 }
